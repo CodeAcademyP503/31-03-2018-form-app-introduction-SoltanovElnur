50f447147dfe8af702152d6864d933aed2aa01d3
SoltanovElnur <elnures@code.edu.az>
2018-04-07 17:42:30 +0400

    Hometasks

---
+++ Tasks/CheasBoard/CheasBoard/Board.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheasBoard
+{
+    public partial class Board : Form
+    {
+        Button btn;
+        int top = 0;
+        int left = 0;
+        int row = 8;
+        int column = 8;
+        int i = 0;
+
+        public Board()
+        {
+            InitializeComponent();
+            this.BackColor = Color.DarkRed;
+            this.Width = 8 * 75 + 17;
+            this.Height = 8 * 75 + 40;
+
+            for (row = 0; row < 8; row++)
+            {
+                for (column = 0; column < 8; column++)
+                {
+
+                    btn = new Button();
+                    btn.Enabled = false;
+                    btn.Height = 75;
+                    btn.Width = 75;
+                    btn.Top = top;
+                    btn.Left = left;
+                    btn.FlatStyle = FlatStyle.Popup;
+                    Controls.Add(btn);
+                    left += btn.Width;
+                    if (i % 2 == 0)
+                    {
+                        btn.BackColor = Color.Black;
+                    }
+                    i++;
+                }
+                i++;
+                top += btn.Width;
+                left = 0;
+            }
+        }
+    }
+}
---
+++ Tasks/CinemaApp/CineApp/CineApp/InterFace.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CineApp
+{
+    public partial class InterFace : Form
+    {
+        public InterFace()
+        {
+            InitializeComponent();
+        }
+        private void button_Click(object sender, EventArgs e)
+        {
+            CinemaHall hall = new CinemaHall();
+            hall.Show();
+            
+
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CinemaHall hall = new CinemaHall();
+            hall.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CinemaHall hall = new CinemaHall();
+            hall.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            CinemaHall hall = new CinemaHall();
+            hall.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            CinemaHall hall = new CinemaHall();
+            hall.Show();
+        }
+
+    }
+}
